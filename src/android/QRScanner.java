57e34a90d9546b8fb5982e04427a7c8a61150f6e
danstokoe <dans@softpauer.com>
2024-07-18 10:24:20 +0100

    add support for data matrix codes

--- src/android/QRScanner.java
+++ src/android/QRScanner.java
@@ -463,6 +463,7 @@ public class QRScanner extends CordovaPlugin implements BarcodeCallback {
                 formatList.add(BarcodeFormat.EAN_13);
                 formatList.add(BarcodeFormat.CODE_128);
                 formatList.add(BarcodeFormat.CODE_39);
+                formatList.add(BarcodeFormat.DATA_MATRIX);
                 mBarcodeView.setDecoderFactory(new DefaultDecoderFactory(formatList, null, null));
 
                 //Configure the camera (front/back)
